4a25169bb496d40001a8f0116c0ab37d03ca368d
André Luis Catharino <43781123+lancryter@users.noreply.github.com>
2018-10-24 12:52:40 -0300

    Add files via upload

---
+++ Aprendendo Python.py
@@ -0,0 +1,89 @@
+
+# coding: utf-8
+
+# ## primeiras aulas de Pyton
+# 
+
+# In[1]:
+
+
+# Para executar as celulas tecle Run ou Shift + enter
+
+
+# # Numeros e operações matematicas
+
+# In[3]:
+
+
+# soma
+4+4
+
+
+# In[4]:
+
+
+# Subtração
+4-3
+
+
+# In[5]:
+
+
+# Multiplicação
+3*3
+
+
+# In[6]:
+
+
+# Divisão
+3/2
+
+
+# In[7]:
+
+
+# Modulo
+10%3
+
+
+# In[8]:
+
+
+# Potencia
+4**2
+
+
+# In[9]:
+
+
+# em operações simples
+
+
+# # função Type
+
+# Utilizar Sempre que tiver duvida sob um objeto 
+# digito type eo objeto entre (  )
+
+# In[12]:
+
+
+type (5)
+
+
+# In[14]:
+
+
+type (5.0)
+
+
+#  int representa numero inteiro e float numero fracionado
+
+# In[17]:
+
+
+a = 'eu sou uma string'
+type(a)
+
+
+# informação deve ser posta entre aspas
---
+++ Atribuição .py
@@ -0,0 +1,63 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+# Valores de operadores
+## Atribuição de Variaveis
+
+
+# In[7]:
+
+
+a = 4
+b = 6
+c = 8
+
+print ("as atribuições sempre começão por letras")
+print ("sempre se lê a igualdade como, recebe")
+print ("a recebe 4 ")
+
+
+# In[3]:
+
+
+# Sendo assim
+
+
+# In[11]:
+
+
+print ("a + b")
+
+
+# In[12]:
+
+
+a + b
+
+
+# In[13]:
+
+
+print ("a + b - C")
+
+
+# In[14]:
+
+
+a + b - c
+
+
+# In[16]:
+
+
+print ("a * c - a * b")  
+
+
+# In[17]:
+
+
+a * c - a * b
+
